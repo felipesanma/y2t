5f104cab6bc265cdcfbf19344bba9e291b82eda2
felipesanma <felipesmsoto>
2023-12-29 14:25:55 -0300

    transcription and json files download

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from youtube_video_processing import YT2text
 import streamlit as st
 import streamlit.components.v1 as components
 from PIL import Image
+import json
+from streamlit_extras.stylable_container import stylable_container
 
 # SETUP ------------------------------------------------------------------------
 favicon = Image.open("favicon.ico")
 st.set_page_config(
-    page_title="PDF Comparison - LLM",
+    page_title="Y2T - Transcription from Youtube Videos",
     page_icon=favicon,
     layout="wide",
     initial_sidebar_state="auto",
@@ -15,14 +17,17 @@ st.set_page_config(
 
 # Sidebar contents ------------------------------------------------------------------------
 with st.sidebar:
-    st.title("LLM - PDF Comparison App")
+    st.title("Y2T - Transcription from Youtube Videos")
     st.markdown(
         """
     ## About
-    This app is an pdf comparison (LLM-powered), built using:
+    This app is an Youtube Video transcriptor, built using:
     - [Streamlit](https://streamlit.io/)
-    - [LangChain](https://python.langchain.com/)
-    - [OpenAI LLM model](https://platform.openai.com/docs/models) 
+    - [Whisper](https://openai.com/research/whisper)
+    - youtube_transcript_api
+    - youtubesearchpython
+    - pytube
+    - tinytag
     """
     )
     st.write(
@@ -55,13 +60,71 @@ Title_html = """
     </style> 
     
     <div class="title">
-        <h1>Super PDF Comparison</h1>
+        <h1>Transcription from Youtube Videos</h1>
     </div>
     """
+st.markdown(
+    "_Get transcripts from videos even if there is no automatically generated from youtube_"
+)
 components.html(Title_html)
 
-video_id = "-24JrpF01PM"
-video_id = "sc3EjrZf4QA"
-video_id = "-24JrpF01PM"
-video_content = YT2text().extract(video_id=video_id)
-print(video_content)
+youtube_url = st.text_input(
+    "Youtube URL, sample: https://www.youtube.com/watch?v=ojQdVM-nbDg",
+    key="youtube_url",
+)
+button1 = st.button("Get Transcription")
+if not st.session_state.get("button1"):
+    st.session_state["button1"] = button1
+
+if st.session_state["button1"]:
+    if not youtube_url:
+        st.warning("Youtube URL is missing")
+        st.stop()
+
+    with st.spinner("Getting Transcription...."):
+        try:
+            st.session_state.video_id = youtube_url.split("=")[-1]
+            st.session_state.video_content = YT2text().extract(
+                video_id=st.session_state.video_id
+            )
+        except Exception as e:
+            st.error("Something went grong...")
+            st.exception(e)
+            st.stop()
+
+    row2_spacer1, row2_1, row2_spacer2, row2_2, row2_spacer3 = st.columns(
+        (0.3, 1.5, 0.3, 1.5, 0.3)
+    )
+    with row2_1:
+        st.video(youtube_url)
+
+    with row2_2:
+        st.header(st.session_state.video_content["title"])
+        st.write(st.session_state.video_content["description"])
+        if st.download_button(
+            "Download Transcription",
+            st.session_state.video_content["transcription"],
+            file_name=f"{st.session_state.video_id}_transcription.txt",
+        ):
+            st.success("Thanks for downloading!")
+
+    st.subheader(":blue[Transcription] :sunglasses:", divider="rainbow")
+
+    with stylable_container(
+        "codeblock",
+        """
+    code {
+        white-space: pre-wrap !important;
+    }
+    """,
+    ):
+        st.code(st.session_state.video_content["transcription"])
+
+    st.subheader("Youtube Video Information as JSON object", divider="blue")
+    if st.download_button(
+        "Download JSON",
+        json.dumps(st.session_state.video_content),
+        file_name=f"{st.session_state.video_id}.json",
+    ):
+        st.success("Thanks for downloading!")
+    st.json(st.session_state.video_content)
